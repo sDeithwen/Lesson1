87f3680aa2b47a068338aa5e433fbf5a4777fed0
sDeithwen <ahmed.askerov@bk.ru>
2022-10-28 19:40:16 +0500

    end of lesson 2

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let userFirstName = "Super";
+let userLastName = "Murloc";
+let userBirthDay = "";
+let userBirthMonth = "";
+let userBirthYear = "";
+let userAge = "";
+function getData() {
+    userFirstName = prompt("Введите имя");
+    userLastName = prompt("Введите фамилию");
+    userBirthYear = prompt("Введите год рождения");
+    userBirthMonth = prompt("Введите месяц рождения в виде числа");
+    userBirthDay = prompt("Введите день рождения");
+    userAge = 2022 - userBirthYear;
+    alert("Я знаю о Вас всё... Вы: " + userLastName + " " + userFirstName
+        + ", Вам столько лет: " + userAge + " лет, ибо день рождения у Вас: "
+        + userBirthDay + "." + userBirthMonth + "." + userBirthYear);
+}
+
+getData();
