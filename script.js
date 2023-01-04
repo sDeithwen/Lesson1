b13d5c7c70cc776d9e1efa3cac0bf87852e14e1a
sDeithwen <ahmed.askerov@bk.ru>
2023-01-04 14:14:04 +0500

    New end of Lesson2(2)

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let userBirthDay = "";
 let userBirthMonth = "";
 let userBirthYear = "";
 let userAge = "";
+
 function getData() {
     userFirstName = prompt("Введите имя");
     userLastName = prompt("Введите фамилию");
@@ -13,6 +14,7 @@ function getData() {
     userBirthMonth = prompt("Введите месяц рождения в виде числа");
     userBirthDay = prompt("Введите день рождения");
     userAge = 2022 - userBirthYear;
+
     alert("Я знаю о Вас всё... Вы: " + userLastName + " " + userFirstName
         + ", Вам столько лет: " + userAge + " лет, ибо день рождения у Вас: "
         + userBirthDay + "." + userBirthMonth + "." + userBirthYear);
