67704872dce1b62e8d8344c474e005ac60f92ff7
sDeithwen <ahmed.askerov@bk.ru>
2023-01-04 14:37:34 +0500

    New end of Lesson8-9

---
+++ DomTasks/DomTask1/script.js
@@ -0,0 +1,48 @@
+let tbl;
+let goodsTable = [
+    ["Название", "Цена", "Действие"],
+    ["Чай", 10],
+    ["Кофе", 30],
+    ["Кола", 45],
+    ["Ведьмак 3", 450],
+    ["Булочка", 60],
+]
+
+function removeRow(rowId) {
+    let row = document.getElementById(rowId);
+    row.remove();
+}
+
+function createButton(rowId) {
+    return `<button class = 'btn btn-danger' onclick ="removeRow('${rowId}')">Удалить</button`;
+}
+
+function createTable() {
+    tbl = document.createElement("table");
+    const tblBody = document.createElement("tbody");
+
+    tbl.classList.add('table');
+    document.body.appendChild(tbl);
+    tbl.appendChild(tblBody);
+
+    for (let i = 0; i < goodsTable.length; i++) {
+        const row = document.createElement("tr");
+        row.id = `row${i}`;
+
+        tblBody.appendChild(row);
+
+        if (i == 0)
+            row.style.fontWeight = 'bold';
+
+        for (let j = 0; j < goodsTable[0].length; j++) {
+            const cell = document.createElement("td");
+
+            if (j == 2 && i != 0)
+                cell.innerHTML = createButton(row.id);
+            else
+                cell.innerText = goodsTable[i][j];
+
+            row.appendChild(cell);
+        }
+    }
+}
---
+++ DomTasks/DomTask3/script.js
@@ -0,0 +1,135 @@
+let goodsTable = [
+    ["Название", "Цена", "Наличие", "Действие"],
+    ["Чай", 4, 10],
+    ["Кофе", 1, 30],
+    ["Кола", 20, 45],
+    ["Ведьмак 3", 5, 450],
+    ["Булочка", 12, 60],
+]
+
+function removeRow(rowId) {
+    let row = document.getElementById(rowId);
+
+    row.remove();
+}
+
+function addRow() {
+    const tblBody = document.getElementsByTagName("tbody")[0];
+    const rows = tblBody.rows;
+    const inputsArr = [document.getElementById('newRowName'), document.getElementById('newRowCost'), document.getElementById('newRowCount')];
+    const row = document.createElement("tr");
+    row.id = `row${parseInt(rows[rows.length - 2].id.substring(3)) + 1}`;
+
+    for (i = 0; i < 4; i++) {
+        const cell = document.createElement("td");
+
+        if (i < 3) {
+            cell.innerText = inputsArr[i].value == null || inputsArr[i].value.trim() == '' ? inputsArr[i].placeholder : inputsArr[i].value.trim();
+            inputsArr[i].value = '';
+
+            if (i === 0)
+                cell.onclick = () => { showFullInfo(row.id) };
+        } else
+            cell.innerHTML = createRemoveButton(row.id);
+
+        row.appendChild(cell);
+    }
+    tblBody.appendChild(row);
+    tblBody.insertBefore(rows[rows.length - 1], rows[rows.length - 2]);
+}
+
+function createRemoveButton(rowId) {
+    return `<button class = 'btn btn-danger' onclick ="removeRow('${rowId}')">Удалить</button`;
+}
+
+function createAddButton() {
+    return `<button class = 'btn btn-primary' onclick ="addRow()">Добавить</button`;
+}
+
+function createFullInfoLabel() {
+    let label;
+    label = document.getElementById('fullInfo');
+
+    if (label == null) {
+        label = document.createElement('div');
+        label.id = 'fullInfo';
+        label.visibility = true;
+        document.body.appendChild(label)
+    }
+
+    return label;
+}
+
+function showFullInfo(rowId) {
+    let row = document.getElementById(rowId);
+    let label = createFullInfoLabel();
+    label.innerHTML = `<strong>Название товара: </strong> ${row.children[0].innerText} </br>` +
+        `<strong>Цена товара: </strong> ${row.children[1].innerText} </br>` +
+        `<strong>Количество на складе: </strong> ${row.children[2].innerText} </br>`;
+}
+
+function createAddRow(tblBody) {
+    const row = document.createElement('tr');
+
+    for (i = 0; i < 4; i++) {
+        td = document.createElement('td');
+        row.appendChild(td);
+
+        if (i < 3) {
+            let input = document.createElement('input');
+            input.classList.add('form-control');
+
+            if (i == 0) {
+                input.type = 'text';
+                input.id = 'newRowName';
+                input.placeholder = 'Новый товар';
+            } else {
+                input.type = 'number';
+
+                if (i == 1) {
+                    input.id = 'newRowCost';
+                    input.placeholder = '10';
+                } else {
+                    input.id = 'newRowCount';
+                    input.placeholder = '50';
+                }
+            }
+            td.appendChild(input);
+        } else
+            td.innerHTML = createAddButton();
+    }
+    tblBody.appendChild(row);
+}
+
+function createTable() {
+    let tbl = document.createElement("table");
+    const tblBody = document.createElement("tbody");
+
+    tbl.classList.add('table');
+    document.body.appendChild(tbl);
+    tbl.appendChild(tblBody);
+
+    for (let i = 0; i < goodsTable.length; i++) {
+        const row = document.createElement("tr");
+        row.id = `row${i}`;
+
+        tblBody.appendChild(row);
+
+        if (i == 0)
+            row.style.fontWeight = 'bold';
+
+        for (let j = 0; j < goodsTable[0].length; j++) {
+            const cell = document.createElement("td");
+
+            if (j == 3 && i != 0)
+                cell.innerHTML = createRemoveButton(row.id);
+            else {
+                cell.innerText = goodsTable[i][j];
+                if (j == 0 && i != 0)
+                    cell.onclick = () => { showFullInfo(row.id) };
+            }
+            row.appendChild(cell);
+        }
+    }
+    createAddRow(tblBody);
+}
